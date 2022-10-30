bc562539be00587288341ec87e0fc3a2914910a7
Bipin Lamichhane <72651245+bipinlam@users.noreply.github.com>
2022-10-30 17:25:10 +1100

    Add files via upload
    Auto email script

---
+++ ExampleEmailScript.ps1
@@ -0,0 +1,18 @@
+﻿. ('C:\Users\bipinl\Desktop\MessageFunction.ps1') #location of message function
+
+ $from = "<bipin.lamichhane@carnivalaustralia.com>"
+ $subject = 'Test'
+ #$file = 'C:\Attachment.txt'
+ $to = "jared.marshall@carnivalaustralia.com"
+
+ $message = New-Object System.Net.Mail.MailMessage $from, $to
+ #$message.cc.Add("bipin.laam@outlook.com")
+ #$message.bcc.Add("noemail@ermail.com")
+ 
+ $message.Subject = "Test"
+ #$message.Attachments.Add($file)
+
+ $message.IsBodyHtml = $true
+ $message.Body = "<HTML><body>Hey Jared thanks for your script!</body></HTML>"
+ 
+ message-send -message $message
---
+++ MessageFunction.ps1
@@ -0,0 +1,19 @@
+﻿function message-send {
+
+param([System.Net.Mail.MailMessage]$message="")
+
+$hostname = hostname 
+$user = [Environment]::UserName #current windows user and email user
+
+#To Change Password
+#"Password123" | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Out-File "\\$hostname\C$\Users\$user\Credentials\EmailPassword.txt"
+
+    $emailPw ="c:\Users\bipinl\Credentials\EmailPassword.txt"
+    $pw = Get-Content $emailPw | ConvertTo-SecureString 
+    
+    $smtp = New-Object Net.Mail.SmtpClient("pocruises_australia")
+    $smtp.Credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $user, $pw
+    $smtp.Send($message)
+}
+
+
